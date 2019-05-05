1432d27cfcd1ae72a1ff7a8e60643e0bed5a9aaf
wangnz <wangnz@qq.com>
2019-05-05 18:00:30 +0800

    upd

--- ok328-srv/src/main/java/srv/controller/TestController.java
+++ ok328-srv/src/main/java/srv/controller/TestController.java
@@ -4,12 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.bind.annotation.*;
 import srv.common.Result;
 import srv.common.ResultFactory;
+import srv.vo.User;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping(value = "/test")
@@ -19,9 +20,20 @@ public class TestController {
     @Autowired
     private Environment env;// 读取环境变量
 
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
     @RequestMapping(value = "/test1", method = RequestMethod.GET)
     @ResponseBody
     public Result test1() {
         return ResultFactory.buildSuccessResult(env.getProperty("spring.application.name"));
+    }
+
+    @RequestMapping(value = "/addUser", method = RequestMethod.GET)
+    @ResponseBody
+    public Result addUser(@RequestBody User user) {
+        user.setCreateTime(new Date());
+        mongoTemplate.insert(user);
+        return ResultFactory.buildSuccessResult(user);
     }
 }
