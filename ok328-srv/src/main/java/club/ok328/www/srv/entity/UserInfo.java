ecd91d25599354e4fe4709e9368e93aa5cefde7f
wangnz <wangnz@qq.com>
2019-07-08 14:26:43 +0800

    upd

---
+++ ok328-srv/src/main/java/club/ok328/www/srv/entity/UserInfo.java
@@ -0,0 +1,73 @@
+package club.ok328.www.srv.entity;
+
+public class UserInfo {
+    private Long id;
+
+    private Long createdAt;
+
+    private Long lastModified;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(Long lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName == null ? null : firstName.trim();
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName == null ? null : lastName.trim();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+}
---
+++ ok328-srv/src/main/java/club/ok328/www/srv/mapper/UserInfoMapper.java
@@ -0,0 +1,17 @@
+package club.ok328.www.srv.mapper;
+
+import club.ok328.www.srv.entity.UserInfo;
+
+public interface UserInfoMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UserInfo record);
+
+    int insertSelective(UserInfo record);
+
+    UserInfo selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(UserInfo record);
+
+    int updateByPrimaryKey(UserInfo record);
+}
