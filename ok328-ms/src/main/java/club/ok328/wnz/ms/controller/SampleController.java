342e3772854b0f2f0547e210968ae15739bdf6d3
wangnz <5323700@qq.com>
2020-12-24 17:05:33 +0800

    upd

---
+++ ok328-ms/src/main/java/club/ok328/wnz/ms/controller/SampleController.java
@@ -0,0 +1,17 @@
+package club.ok328.wnz.ms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/demo")
+public class SampleController {
+
+    @RequestMapping("/thymeleaf")
+    public String thymeleaf(Model model) {
+        model.addAttribute("name", "test");
+        return "hello";
+    }
+
+}
